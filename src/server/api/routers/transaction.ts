36257affeb1c3b9e6be7b5da681bfdf7e7f79c40
Afrie Irham <afrieirham.work@gmail.com>
2024-06-26 22:54:05 +0800

    update balance record on add purchase

--- src/server/api/routers/transaction.ts
+++ src/server/api/routers/transaction.ts
@@ -6,7 +6,30 @@ export const transactionRouter = createTRPCRouter({
   createPurchase: publicProcedure
     .input(z.object({ price: z.number(), quantity: z.number() }))
     .mutation(async ({ ctx, input }) => {
-      return ctx.db.purchases.create({
+      // get latest balance record
+      const [latestBalance] = await ctx.db.balance.findMany({ take: -1 });
+
+      // add new balance record
+      const amount = input.price * input.quantity;
+
+      if (!latestBalance) {
+        await ctx.db.balance.create({
+          data: {
+            amount: amount,
+            quantity: input.quantity,
+          },
+        });
+      } else {
+        await ctx.db.balance.create({
+          data: {
+            amount: latestBalance.amount + amount,
+            quantity: latestBalance.quantity + input.quantity,
+          },
+        });
+      }
+
+      // add purchase record
+      return await ctx.db.purchases.create({
         data: {
           price: input.price,
           quantity: input.quantity,
