5d9611dcae0e609910de78483f77a73c85412b9d
Afrie Irham <afrieirham.work@gmail.com>
2024-06-30 23:39:23 +0800

    update test case for create sales and purchase

--- src/__test__/transaction.test.ts
+++ src/__test__/transaction.test.ts
@@ -1,7 +1,8 @@
-import { describe, expect, test, afterEach, beforeEach } from "@jest/globals";
+import { afterEach, beforeEach, describe, expect, test } from "@jest/globals";
 
-import { createInnerTRPCContext } from "~/server/api/trpc";
+import { TransactionType } from "~/constant";
 import { appRouter } from "~/server/api/root";
+import { createInnerTRPCContext } from "~/server/api/trpc";
 import { db } from "~/server/db";
 
 const ctx = createInnerTRPCContext({});
@@ -18,137 +19,93 @@ beforeEach(reset);
 afterEach(reset);
 
 describe("transactions", () => {
-  test("empty transaction", async () => {
-    const transactions = await caller.transaction.getAllTransactions();
-    expect(transactions).toEqual([]);
-  });
-
-  test("testing add random order, delete, update", async () => {
-    // add correct purchase 150 2
+  test("add new purchase", async () => {
     const first = await caller.transaction.createPurchase({
       cost: 2,
       quantity: 150,
       position: 0,
     });
-    if (!first) return;
+    if (!first) return expect(true).toBe(false);
 
-    await expectRecordToBe({
-      totalTransaction: 1,
+    await expectTransactionToBe({
+      type: TransactionType.Purchase,
+      quantity: 150,
+      cost: "2.00",
+
       totalQuantity: 150,
       totalAsset: "300.00",
       totalCostPerUnit: "2.00",
-    });
 
-    // add wrong sale position
-    const wrongPosition = await caller.transaction.createSale({
-      quantity: -5,
-      position: 0,
-    });
-    if (!wrongPosition) return;
-
-    await expectRecordToBe({
-      totalTransaction: 2,
-      totalQuantity: 145,
-      totalAsset: "290.00",
-      totalCostPerUnit: "2.00",
-    });
-
-    // add correct purchase 10 1.5
-    await caller.transaction.createPurchase({
-      cost: 1.5,
-      quantity: 10,
-      position: 0,
-    });
-
-    await expectRecordToBe({
-      totalTransaction: 3,
-      totalQuantity: 155,
-      totalAsset: "305.00",
-      totalCostPerUnit: "1.97",
-    });
-
-    // delete wrongPosition sale
-    await caller.transaction.deleteTransaction({ id: wrongPosition.id });
-
-    await expectRecordToBe({
-      totalTransaction: 2,
-      totalQuantity: 160,
-      totalAsset: "315.00",
-      totalCostPerUnit: "1.97",
-    });
-
-    // add wrong sale 10
-    const wrongQuantity = await caller.transaction.createSale({
-      quantity: -10,
-      position: 0,
-    });
-    if (!wrongQuantity) return;
-
-    await expectRecordToBe({
-      totalTransaction: 3,
-      totalQuantity: 150,
-      totalAsset: "295.31",
-      totalCostPerUnit: "1.97",
-    });
-
-    // update wrong sale to 5
-    await caller.transaction.updateTransaction({
-      id: wrongQuantity.id,
-      quantity: -5,
-      cost: Number(wrongQuantity.cost),
-      type: wrongQuantity.type,
-    });
-
-    await expectRecordToBe({
-      totalTransaction: 3,
-      totalQuantity: 155,
-      totalAsset: "305.16",
-      totalCostPerUnit: "1.97",
-    });
-
-    // delete first transaction
-    await caller.transaction.deleteTransaction({ id: first.id });
-
-    await expectRecordToBe({
-      totalTransaction: 2,
-      totalQuantity: 5,
-      totalAsset: "7.50",
-      totalCostPerUnit: "1.50",
-    });
-
-    // add first transaction again
-    await caller.transaction.createPurchase({
-      cost: 2,
-      quantity: 150,
-      position: -1,
-    });
-    await expectRecordToBe({
-      totalTransaction: 3,
-      totalQuantity: 155,
-      totalAsset: "305.16",
-      totalCostPerUnit: "1.97",
+      totalTransaction: 1,
     });
   });
 });
 
-const expectRecordToBe = async ({
-  totalTransaction,
+test("add new sale", async () => {
+  await caller.transaction.createPurchase({
+    cost: 2,
+    quantity: 150,
+    position: 0,
+  });
+  await caller.transaction.createPurchase({
+    cost: 1.5,
+    quantity: 10,
+    position: 0,
+  });
+  // if (!purchase) return expect(true).toBe(false);
+
+  const sale = await caller.transaction.createSale({
+    quantity: -5,
+    position: 0,
+  });
+  if (!sale) return expect(true).toBe(false);
+
+  await expectTransactionToBe({
+    type: TransactionType.Sale,
+    quantity: -5,
+    cost: "1.97",
+
+    totalQuantity: 155,
+    totalAsset: "305.16",
+    totalCostPerUnit: "1.97",
+
+    totalTransaction: 3,
+  });
+});
+
+const expectTransactionToBe = async ({
+  type,
+  quantity,
+  cost,
+
   totalQuantity,
   totalAsset,
   totalCostPerUnit,
+
+  totalTransaction,
 }: {
-  totalTransaction: number;
+  type: string;
+  quantity: number;
+  cost: string;
+
   totalQuantity: number;
   totalAsset: string;
   totalCostPerUnit: string;
+
+  totalTransaction: number;
 }) => {
   const transactions = await caller.transaction.getAllTransactions();
   const last = await caller.transaction.getLatestUnitCost();
   if (!last) {
     return;
   }
-  expect(transactions.length).toEqual(totalTransaction);
+  expect(last.type).toEqual(type);
+  expect(last.quantity).toEqual(quantity);
+  expect(last.cost.toFixed(2)).toEqual(cost);
+
   expect(last.totalQuantity).toEqual(totalQuantity);
   expect(last.totalAsset.toFixed(2)).toEqual(totalAsset);
   expect(last.costPerUnit.toFixed(2)).toEqual(totalCostPerUnit);
+
+  expect(transactions.length).toEqual(totalTransaction);
 };
